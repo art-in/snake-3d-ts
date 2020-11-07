f21e0ccc0105bffbe8fbffd1fe99d06f46f79ed0
art-in <artin@artin.space>
2020-11-07 09:21:16 +0000

    setup infrastructure
    - eject from create-react-app --template typescript
    - add prettier / customize eslint config
    - make .css default extension for CSS modules (instead of .module.css) / remove hash from generated class name

---
+++ src/components/App/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './App.css';
+
+export default function App(): JSX.Element {
+  return <div className={classes.root}>snake-3d-ts</div>;
+}
---
+++ src/declarations.d.ts
@@ -0,0 +1,32 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly PUBLIC_URL: string;
+  }
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {title?: string}
+  >;
+
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const classes: {readonly [key: string]: string};
+  export default classes;
+}
